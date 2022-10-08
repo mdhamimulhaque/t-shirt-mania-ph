3e246632281c18b78c5a0b087fc691c4dc13943f
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-08 10:51:34 +0600

    router setup with header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Header.css";
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    const navItems = [
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "Orders", path: "/orders" },
+        { id: 3, name: "About", path: "/about" },
+    ]
+    return (
+        <header>
+            <div className="logo">
+                <h2>Logo</h2>
+            </div>
+            <nav className='nav_wrapper'>
+                {
+                    navItems.map(navItem =>
+                        <Link to={navItem.path} key={navItem.id} >
+                            {navItem.name}
+                        </Link>
+
+                    )
+                }
+            </nav>
+        </header >
+    );
+};
+
+export default Header;
